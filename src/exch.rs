02e2be2eef9a80c79718997313bc30f8f72e25a7
William Alhant <wi.alhant@gmail.com>
2024-03-30 00:52:24 +0100

    Syscall version

--- src/exch.rs
+++ src/exch.rs
@@ -1,7 +1,9 @@
-use nix::fcntl::{renameat2, RenameFlags};
+use syscalls::{syscall, Sysno};
+use std::os::unix::ffi::OsStrExt;
 use std::path::PathBuf;
+use std::ffi::CString;
 use nix::libc::AT_FDCWD;
-use nix::Result;
+use nix::fcntl::RenameFlags;
 use clap::Parser;
 
 #[derive(Parser)]
@@ -14,11 +16,22 @@ struct Cli {
     new: PathBuf,
 }
 
-fn main() -> Result<()> {
+fn main() -> Result<(), syscalls::Errno> {
     let cli = Cli::parse();
-    let res = renameat2(Some(AT_FDCWD), &cli.old, Some(AT_FDCWD), &cli.new, RenameFlags::RENAME_EXCHANGE);
+    let old = CString::new(cli.old.as_os_str().as_bytes()).unwrap();
+    let new = CString::new(cli.new.as_os_str().as_bytes()).unwrap();
+    let res = unsafe {
+        syscall!(
+            Sysno::renameat2,
+            AT_FDCWD,
+            old.as_ptr(),
+            AT_FDCWD,
+            new.as_ptr(),
+            RenameFlags::RENAME_EXCHANGE.bits()
+        )
+    };
     if let Err(e) = res {
         eprintln!("Error: {}", e);
     }
-    res
+    res.map(|_| ())
 }
